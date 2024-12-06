583590db19e644f0d9fab657d8e00e8908e3a270
Clemmy Liao <clemmy.liao@Clemmys-Laptop.local>
2024-12-06 20:22:19 +0800

    Modify comment

--- models/post.js
+++ models/post.js
@@ -1,28 +1,28 @@
 const db = require('../config/db');
 
 const Post = {
-  // 獲取所有帖子
+  // 獲取所有貼文
   getAll: async () => {
     const query = 'SELECT * FROM posts';
     const [rows] = await db.promise().query(query);
     return rows;
   },
 
-  // 創建新帖子
+  // 創建新貼文
   create: async (name) => {
     const query = 'INSERT INTO posts (name) VALUES (?)';
     const [result] = await db.promise().query(query, [name]);
-    return result.insertId; // 返回新插入的帖子 ID
+    return result.insertId; // 返回新插入的貼文 ID
   },
 
-  // 更新帖子
+  // 更新貼文
   update: async (id, name) => {
     const query = 'UPDATE posts SET name = ? WHERE id = ?';
     const [result] = await db.promise().query(query, [name, id]);
     return result.affectedRows; // 返回受影響的行數
   },
 
-  // 刪除帖子
+  // 刪除貼文
   delete: async (id) => {
     const query = 'DELETE FROM posts WHERE id = ?';
     const [result] = await db.promise().query(query, [id]);
