31773afb3b3dba1637c43c54ea15390e01a64c55
Clemmy Liao <clemmy.liao@Clemmys-Laptop.local>
2024-12-08 19:25:03 +0800

    Remove code for testing

--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -12,22 +12,11 @@ const csrfErrorHandler = (err, req, res, next) => {
 
 // 錯誤處理中間件
 const errorHandler = (err, req, res, next) => {
-  // 那這邊的Logger不用嗎？
-  // 不用加密？
   // 記錄錯誤資訊
   logService.error(`錯誤訊息: ${err.message}`);
   logService.error(`錯誤堆疊: ${err.stack}`);
 
-  // 如果是開發環境，回傳完整錯誤訊息
-  if (process.env.NODE_ENV !== 'production') {
-    return res.status(500).json({
-      error: '伺服器錯誤',
-      message: err.message,
-      stack: err.stack,
-    });
-  }
-
-  // 生產環境，僅返回簡化錯誤訊息
+  // 返回簡化錯誤訊息
   res.status(500).json({
     error: '伺服器錯誤，請稍後再試。',
   });
